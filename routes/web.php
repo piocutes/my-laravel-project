67a54a01f788f75d31c33d65ab60363aafd786d0
piocutes <patrickpioquinto@student.laverdad.edu.ph>
2024-10-23 00:56:07 +0800

    Revert "Adding Authentication."
    This reverts commit d2bee8ad95e7bd0ea6ce8f90b10e5ec948621a86.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
 
@@ -8,22 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {    
-    Route::get("/notes", [NoteController::class, "index"])->name("notes.index");
-    Route::get("/notes/create", [NoteController::class, "create"])->name("notes.create");
-    Route::post("/notes", [NoteController::class, "store"])->name("notes.store");
-    Route::get("/notes/{note}/edit", [NoteController::class, "edit"])->name("notes.edit");
-    Route::get("/notes/{note}/delete", [NoteController::class, "delete"])->name("notes.delete"); 
-    Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
-    Route::delete('/notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
-    
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+Route::get("/notes", [NoteController::class, "index"])->name("notes.index");
+Route::get("/notes/create", [NoteController::class, "create"])->name("notes.create");
+Route::post("/notes", [NoteController::class, "store"])->name("notes.store");
+Route::get("/notes/{note}/edit", [NoteController::class, "edit"])->name("notes.edit");
+Route::get("/notes/{note}/delete", [NoteController::class, "delete"])->name("notes.delete"); 
+Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
+Route::delete('/notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
